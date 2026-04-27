5e5f04d40fce89bdbd84c3801b2e303dcffd588b
Nicolo Stanciu <iphone5001@gmail.com>
2026-04-27 15:51:37 +0100

    wipe old page

--- Sources/Site/Main.swift
+++
@@ -1,9 +0,0 @@
-import SiteKit
-
-@main struct Site {
-   static func main() throws {
-      try SiteBuilder.portfolio(configPath: "SiteConfig.yaml")
-         .replacing(HomePageRenderer.self, with: LandingPageRenderer())
-         .run()
-   }
-}
