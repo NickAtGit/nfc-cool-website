d2cb8a0fd350bca69c50a49a3b19f91ed879a6c0
Robin Wonderboy <>
2026-04-27 11:06:32 +0100

    Minimal config matching blog format, remove LandingPageRenderer temporarily for debugging

--- Sources/Site/Main.swift
+++ Sources/Site/Main.swift
@@ -5,11 +5,9 @@ import SiteKit
    static func main() throws {
       do {
          try SiteBuilder.portfolio(configPath: "SiteConfig.yaml")
-            .replacing(HomePageRenderer.self, with: LandingPageRenderer())
             .renderer(StaticPageRenderer())
             .renderer(SitemapRenderer())
             .renderer(RobotsTxtRenderer())
-            .renderer(HTMLRedirectPageRenderer())
             .run()
       } catch {
          FileHandle.standardError.write(Data("Error: \(error)\n".utf8))
