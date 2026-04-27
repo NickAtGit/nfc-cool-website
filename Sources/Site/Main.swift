ffb2bee149def375b221a58624761caeb93e5b75
Robin Wonderboy <>
2026-04-27 11:12:48 +0100

    Re-add LandingPageRenderer (base build works)

--- Sources/Site/Main.swift
+++ Sources/Site/Main.swift
@@ -3,15 +3,11 @@ import SiteKit
 
 @main struct Site {
    static func main() throws {
-      do {
-         try SiteBuilder.portfolio(configPath: "SiteConfig.yaml")
-            .renderer(StaticPageRenderer())
-            .renderer(SitemapRenderer())
-            .renderer(RobotsTxtRenderer())
-            .run()
-      } catch {
-         FileHandle.standardError.write(Data("Error: \(error)\n".utf8))
-         throw error
-      }
+      try SiteBuilder.portfolio(configPath: "SiteConfig.yaml")
+         .replacing(HomePageRenderer.self, with: LandingPageRenderer())
+         .renderer(StaticPageRenderer())
+         .renderer(SitemapRenderer())
+         .renderer(RobotsTxtRenderer())
+         .run()
    }
 }
